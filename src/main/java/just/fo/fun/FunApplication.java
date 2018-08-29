c4acc27a2c55fc3f68d168c1599c6e9d4f8b6c2b
skynet <b.seilov@infin-it.kz>
2018-08-29 21:52:05 +0300

    ok

--- src/main/java/just/fo/fun/FunApplication.java
+++ src/main/java/just/fo/fun/FunApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"just.fo.fun", "just.fo.fun.auth",  "just.fo.fun.auth.controller"})
+//@ComponentScan(basePackages = {"just.fo.fun", "just.fo.fun.auth",  "just.fo.fun.auth.controller"})
 @SpringBootApplication
 
 public class FunApplication {
