81f8be865d36ea01d3dbbb487f4d9f506f4ded97
skynet <b.seilov@infin-it.kz>
2018-10-12 10:13:27 +0300

    translations

---
+++ src/main/java/just/fo/fun/category/controller/CategoryController.java
@@ -0,0 +1,99 @@
+package just.fo.fun.category.controller;
+
+import just.fo.fun.category.service.CategoryService;
+import just.fo.fun.entities.Category;
+import just.fo.fun.entities.User;
+import just.fo.fun.exception.MessageException;
+import just.fo.fun.user.models.dtos.UserDto;
+import just.fo.fun.user.service.UserService;
+import just.fo.fun.utils.Utils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity insertUser(@Valid @RequestBody final Category category) {
+
+        if (category.getId() != null)
+            throw new MessageException("id must be empty !");
+
+        Category result;
+        try {
+            result = categoryService.save(category);
+        }catch (Exception e){
+            throw new MessageException("ffffffff" + e.getMessage());
+        }
+        return result == null
+                ? new ResponseEntity<>(HttpStatus.CONFLICT)
+                : new ResponseEntity<>(result, HttpStatus.OK);
+
+    }
+/*
+    @PutMapping
+    public ResponseEntity updateUser(@Valid @RequestBody final UserDto userDto) {
+
+        if (userDto.getId() == null)
+            throw new MessageException("id must not be empty !");
+        User user = new User();
+        BeanUtils.copyProperties(userDto, user);
+        User resultUser = userService.save(user);
+        return resultUser == null
+                ? new ResponseEntity<>(HttpStatus.CONFLICT)
+                : new ResponseEntity<>(resultUser, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getUser(@PathVariable final Long id) {
+
+        UserDto userDto = new UserDto();
+        User user = userService.findOne(id);
+
+        BeanUtils.copyProperties(user, userDto);
+        return userDto == null
+                ? new ResponseEntity<>(HttpStatus.CONFLICT)
+                : new ResponseEntity<>(userDto, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/")
+    public ResponseEntity getUsers() {
+
+        List<User> users = userService.findAll();
+
+        List<UserDto> resultUserDto = users.stream().map(itm -> {
+            UserDto userDto = new UserDto();
+            BeanUtils.copyProperties(itm, userDto);
+            return userDto;
+        }).collect(Collectors.toList());
+
+        return resultUserDto == null
+                ? new ResponseEntity<>(HttpStatus.CONFLICT)
+                : new ResponseEntity<>(resultUserDto, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete (@PathVariable final Long id) {
+        userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+*/
+
+}
